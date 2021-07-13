4552574e81cf546b71c6d0b8a0aaae196d893817
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 11:30:01 +0200

    was geändert

--- src/main/java/la/FarylizerPlugin.java
+++ src/main/java/la/FarylizerPlugin.java
@@ -12,8 +12,6 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import sun.tools.jconsole.Plotter;
-
 public class FarylizerPlugin extends JavaPlugin {
 
 	@Override
@@ -26,14 +24,24 @@ public class FarylizerPlugin extends JavaPlugin {
 		getLogger().info("Kommando aufgerufen:" + cmd.getName());
 		Player player = (Player) sender;
 
+		System.out.println("Test");
+
 		if (cmd.getName().equalsIgnoreCase("getLocation")) {
 			holeEigenerStandort(player);
 			holeZielStandort(player);
 		}
 
 		if (cmd.getName().equalsIgnoreCase("DeleteBlock")) {
-			löscheBlock(player);
-			gugunlerCube(player.getLocation(), 0, 0, 0);
+
+			gugunlerCube(player.getLocation(), 0, 0, 0, "EAST", player);
+			String direction = testeMethode(player);
+			if (direction.equalsIgnoreCase("EAST")) {
+//				löscheBlockEast(player);
+			}
+			if (direction.equalsIgnoreCase("WEST")) {
+				löscheBlockWest(player);
+			}
+
 		}
 
 		return true;
@@ -59,7 +67,7 @@ public class FarylizerPlugin extends JavaPlugin {
 		return null;
 	}
 
-	public void gugunlerCube(Location loc, int length, int breite, int Höhe) {
+	public void gugunlerCube(Location loc, int length, int breite, int Höhe, String direction, Player schütze) {
 
 		int x1 = loc.getBlockX();
 		int y1 = loc.getBlockY();
@@ -67,66 +75,91 @@ public class FarylizerPlugin extends JavaPlugin {
 		getLogger().info("X ---> " + x1);
 		getLogger().info("Y ---> " + y1);
 		getLogger().info("Z ---> " + z1);
+
+		if (direction == "EAST") {
+
+//	  löscheBlockEast(schütze) {
+//		  while(direction == EAST) {
+//	  }
+			Block currentBlock1 = schütze.getTargetBlock(null, 0);
+			getLogger().info("Block: " + currentBlock1.toString());
+			currentBlock1.setType(Material.AIR);
+
+			Block currentBlock2 = currentBlock1.getRelative(-1, 0, 0);
+			getLogger().info("Block: " + currentBlock2.toString());
+			currentBlock2.setType(Material.AIR);
+
+			Block currentBlock3 = currentBlock1.getRelative(1, 0, 0);
+			getLogger().info("Block: " + currentBlock3.toString());
+			currentBlock2.setType(Material.AIR);
+
+			Block currentBlock4 = currentBlock1.getRelative(0, 1, 0);
+			getLogger().info("Block: " + currentBlock4.toString());
+			currentBlock4.setType(Material.AIR);
+
+			Block currentBlock5 = currentBlock1.getRelative(0, -1, 0);
+			getLogger().info("Block: " + currentBlock5.toString());
+			currentBlock5.setType(Material.AIR);
+
+			Block currentBlock6 = currentBlock1.getRelative(0, 0, 1);
+			getLogger().info("Block: " + currentBlock6.toString());
+			currentBlock6.setType(Material.AIR);
+
+			Block currentBlock7 = currentBlock1.getRelative(0, 0, -1);
+			getLogger().info(currentBlock7.toString());
+			currentBlock7.setType(Material.AIR);
+
+			Block currentBlock8 = currentBlock1.getRelative(1, 0, -1);
+			getLogger().info("Block: " + currentBlock8.toString());
+			currentBlock8.setType(Material.AIR);
+
+			Block currentBlock9 = currentBlock1.getRelative(1, 0, 1);
+			getLogger().info("Block: " + currentBlock9.toString());
+			currentBlock9.setType(Material.AIR);
+
+			Block currentBlock10 = currentBlock1.getRelative(1, 1, 0);
+			getLogger().info("Block: " + currentBlock10.toString());
+			currentBlock10.setType(Material.AIR);
+
+			Block currentBlock11 = currentBlock1.getRelative(1, -1, 0);
+			getLogger().info("Block: " + currentBlock11.toString());
+			currentBlock11.setType(Material.AIR);
+			getLogger().info("Block wurde gelöscht ");
+		}
 	}
 
-	private void löscheBlock(Player player) {
-		Block currentBlock1 = player.getTargetBlock(null, 0);
-		getLogger().info("Block: " + currentBlock1.toString());
-		currentBlock1.setType(Material.STONE);
+	private void löscheBlockWest(Player player) {
+		Material zielMaterial = Material.AIR;
+		Block startBlock = player.getTargetBlock(null, 0);
+		setzeBlock(startBlock, zielMaterial);
+		setzeBlock(startBlock.getRelative(-1, 0, 0), zielMaterial);
+		setzeBlock(startBlock.getRelative(1, 0, 0), zielMaterial);
+		setzeBlock(startBlock.getRelative(0, 1, 0), zielMaterial);
+		setzeBlock(startBlock.getRelative(0, -1, 0), zielMaterial);
+		setzeBlock(startBlock.getRelative(0, 0, 1), zielMaterial);
+		setzeBlock(startBlock.getRelative(0, 0, -1), zielMaterial);
+		setzeBlock(startBlock.getRelative(1, 0, -1), zielMaterial);
+		setzeBlock(startBlock.getRelative(1, 0, 1), zielMaterial);
+		setzeBlock(startBlock.getRelative(1, 1, 0), zielMaterial);
+		setzeBlock(startBlock.getRelative(1, -1, 0), zielMaterial);
+		getLogger().info("Schuss abgegeben");
+	}
 
-		Block currentBlock2 = currentBlock1.getRelative(-1, 0, 0);
-		getLogger().info("Block: " + currentBlock2.toString());
-		currentBlock2.setType(Material.STONE);
-
-		Block currentBlock3 = currentBlock1.getRelative(1, 0, 0);
-		getLogger().info("Block: " + currentBlock3.toString());
-		currentBlock3.setType(Material.AIR);
-
-		Block currentBlock4 = currentBlock1.getRelative(0, 1, 0);
-		getLogger().info("Block: " + currentBlock4.toString());
-		currentBlock4.setType(Material.AIR);
-
-		Block currentBlock5 = currentBlock1.getRelative(0, -1, 0);
-		getLogger().info("Block: " + currentBlock5.toString());
-		currentBlock5.setType(Material.AIR);
-
-		Block currentBlock6 = currentBlock1.getRelative(0, 0, 1);
-		getLogger().info("Block: " + currentBlock6.toString());
-		currentBlock6.setType(Material.AIR);
-
-		Block currentBlock7 = currentBlock1.getRelative(0, 0, -1);
-		getLogger().info(currentBlock7.toString());
-		currentBlock7.setType(Material.AIR);
-
-		Block currentBlock8 = currentBlock1.getRelative(1, 0, -1);
-		getLogger().info("Block: " + currentBlock8.toString());
-		currentBlock8.setType(Material.AIR);
-
-		Block currentBlock9 = currentBlock1.getRelative(1, 0, 1);
-		getLogger().info("Block: " + currentBlock9.toString());
-		currentBlock9.setType(Material.AIR);
-
-		Block currentBlock10 = currentBlock1.getRelative(1, 1, 0);
-		getLogger().info("Block: " + currentBlock10.toString());
-		currentBlock10.setType(Material.AIR);
-
-		Block currentBlock11 = currentBlock1.getRelative(1, -1, 0);
-		getLogger().info("Block: " + currentBlock11.toString());
-		currentBlock11.setType(Material.AIR);
-		getLogger().info("Block wurde gelöscht ");
+	private void setzeBlock(Block block, Material material) {
+		getLogger().info("Lösche Block: " + block.toString());
+		block.setType(material);
 	}
 
 	private String testeMethode(Player spezifischePlayer) {
-	    int yaw = Float.floatToIntBits(spezifischePlayer.getLocation().getYaw());
-	    if(yaw >= 45 && yaw < 135)
-	       return "South";
-	    if(yaw >= 135 && yaw < 180)
-	       return "West";
-	
-	    if(yaw >= 180 && yaw < 225)
-	       return "Noth";
-	
-	    return "East";
+		int yaw = Float.floatToIntBits(spezifischePlayer.getLocation().getYaw());
+		if (yaw >= 45 && yaw < 135)
+			return "South";
+		if (yaw >= 135 && yaw < 180)
+			return "West";
+		if (yaw >= 180 && yaw < 225)
+			return "Noth";
+		else
+			return "East";
 	}
 
 }
